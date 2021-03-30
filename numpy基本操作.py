d3d4a1fe903c8ee02e3678f81c77647d9fc68e28
Ziqing Yan <80880595+ZiqingY@users.noreply.github.com>
2021-03-30 19:06:15 +0100

    Add files via upload

--- numpy基本操作.py
+++ numpy基本操作.py
@@ -5,16 +5,16 @@ Spyder Editor
 This is a temporary script file.
 """
 
-######## Ipython: Python的交互式调试环境（就是右边展示输出结果那一块）#######
+''''Ipython: Python的交互式调试环境（就是右边展示输出结果那一块）'''
 # 在变量后面加？显示变量的相关信息
 # In Out 分别表示输入指令和输出指令
 # 魔术命令例子：%run xx.py 运行xx.py程序...
 # %magic 显示所有IPython的魔术命令
 
 
-###########################      numpy库     ###########################
-import numpy as np
 
+''''numpy库'''
+import numpy as np
 # 展示一下numpy的强大功能
 def pySum():
     a=[0,1,2,3,4]
@@ -30,39 +30,40 @@ def npSum():
     b=np.array([9,8,7,6,5])
     c=a**2+b**3
     return c
-print(npSum())
-# 比上面pySum的语法简单得多
+print(npSum())      #比上面pySum的语法简单得多
 
 
-#____________ndarray数组属性______________
-print(" ")
+'''ndarray数组属性'''
+print()
 x=np.array([[0,1,2,3,4],[5,6,7,8,9]])
 print(x)
-print(x.ndim) #维度
-print(x.shape) #多少行多少列
-print(x.size) #元素个数，即行乘以列
-print(x.dtype) #元素类型
-print(x.itemsize) #每个元素的大小（单位为字节）
+print(x.ndim)       #维度
+print(x.shape)      #多少行多少列
+print(x.size)       #元素个数，即行乘以列
+print(x.dtype)      #元素类型
+print(x.itemsize)   #每个元素的大小（单位为字节）
 
 
 
-# ndarray中的元素类型：bool, intc, inp, int8, int16...（很多很多类型）
+'''ndarray中的元素类型：bool, intc, inp, int8, int16...（很多很多类型）'''
 # ndarray也可以由非同质对象组成，但会被强行转化成同质，无法有效发挥numpy优势。例：
-print(" ")
+print()
 y=np.array([[0,1,2,3,4],[1,2,3,4]])
-print(y.shape) #变成1行1列
-print(y.size) #元素个数变成2个
-print(y.dtype) #元素类型为object
-print(y.itemsize) #每个元素的大小（单位为字节），发现为8字节，较大
+print(y.shape)      #变成1行1列
+print(y.size)       #元素个数变成2个
+print(y.dtype)      #元素类型为object
+print(y.itemsize)   #每个元素的大小（单位为字节），发现为8字节，较大
 
 
 
-
-#_____________ 创建和变幻ndarray_______________________
+'''创建和变幻ndarray'''
 # 1.x=np.array() 括号中输入list[], tuple(), list[]&tuple()都可,只要维度相同就行
+print()
+boris = np.array([ [1,2], [9,8], (0.1,0.2) ], dtype=np.int32)
+print(boris)
 
 # 2.1 np.arange(n)  np.ones(shape)  np.zeros(shape)  np.zeros(shape)  np.full(shape,value)   np.full(shape,value)  np.eye(n)
-print(" ")
+print()
 print(np.arange(10))
 print(np.ones((1,10)))
 print(np.zeros((10,1)))
@@ -70,8 +71,11 @@ print(np.full((4,4),99999))
 print(np.eye(6))
 
 # 2.2 np.ones_like(x)  np.zeros_like(x)  np.full_like(x,value) #根据给定数组生成同形状数组
+print()
+print(np.full_like(boris, 250))
 
 # 2.3 np.linspace()均匀取值  np.concatenate()合并数组
+print()
 a=np.linspace(1,10,4)
 print(a)
 b=np.linspace(1,10,4,endpoint=False)
@@ -80,11 +84,13 @@ print(np.concatenate((a,b)))
 
 # 3.1  .reshape(shape)     .resize(shape)     .swapaxes(ax1,ax2)      .flatten()
 # reshape和flatten生成新数组不改变原数组，resize改变原数组
+print()
 print(x)
 print(x.flatten())
 print(x.reshape(5,2))
 
-# 3.2 new_x=x.astype(new_type) 改变数组类型
+# 3.2 new_x=x.astype(new_type) 改变数组类型; 若和原类型一样，则相当于拷贝
+print()
 w=np.ones((2,3,4),dtype=np.int)
 print(w)
 print(w.astype(np.float))
@@ -97,38 +103,43 @@ print(v.flatten())
 
 
 
-#_____________对ndarray的操作__________________
+'''对ndarray的操作'''
 q=np.array([9,8,7,6,5])
-print(q[2])  #q中的第三个元素
-q[1:4:2] #起始编号 到 终止编号（不含）之间，间隔为2的元素组成组成的ndarray
+print(q[2])           #q中的第三个元素
+q[1:4:2]              #起始编号 到 终止编号（不含）之间，间隔为2的元素组成组成的ndarray
 print(q[1:4:2]) 
-# 切片操作可以用冒号选取整个维度，双冒号表示用特定步长跳跃切片
+                      # 切片操作可以用冒号选取整个维度，双冒号表示用特定步长跳跃切片
 
 r=np.arange(24).reshape((2,3,4))
 print(r)
-print(r[-1,-2,-3]) # 倒数的元素
-print(r[:,1,-3]) # 覆盖第一个维度,只选取满足 在第二个维度1位置和第三个维度-3位置 的元素
-print(r[:, 1:3, :]) # 覆盖第一、三维度，只选取满足 在第二个维度1、2位置 的元素
-print(r[:, :, ::2]) # 覆盖一、二维度，只在三维度进行间隔为2的切片
+print(r[1, 2, 3])
+print(r[-1, -2, -3])  # 倒数的元素
+print(r[:, 1, -3])    # 覆盖第一个维度,只选取满足 在第二个维度1位置和第三个维度-3位置 的元素
+print(r[:, 1:3, :])   # 覆盖第一、三维度，只选取满足 在第二个维度1、2位置 的元素
+print(r[:, :, ::2])   # 覆盖一、二维度，只在三维度进行间隔为2的切片
+print(r.mean())       # 求所有元素均值
 
-print(r.mean()) # 求所有元素均值
 
 
-#________________numpy对ndarray元素的计算_______________
+'''numpy对ndarray元素的计算'''
 # np.square(x)  np.sqrt(x)  np.abs(x)  np.fabs(x)  np.log(x)  np.ceil(x)  np.floor(x)
 # np.rint(x)  np.modf(x)   np.cos/sin/tan(x)  np.exp(x)   np.sign(x)
-print(" ")
+print()
 print(np.square(r))
 print(np.sqrt(r))
 s=np.sqrt(r)
-print(np.modf(s)) #区分整数和小数部分,生成两个数组
+print(np.modf(s))     #区分整数和小数部分,生成两个数组
 
 
 
-#___________________数组x和数组y的二元运算________________
+'''数组x和数组y的二元运算'''
 # x+y  x-y  x*y  x/y  x**y  np.maximum(x,y)  np.minimum(x,y)  np.mod(x,y)  np.copysign(x,y)  算数比较(产生True or False)
+print()
 t=np.arange(24).reshape((2,3,4))
 o=np.sqrt(t)
+print(t)
+print(np.around(o, decimals=2, out=None))
+                      #小数点后面保留两位
 print(np.maximum(t,o))
 print(t>o)
 
